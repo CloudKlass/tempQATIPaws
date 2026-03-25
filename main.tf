3af2bda1b265eea071fccc57367b46a6f0acebbc
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2026-03-25 11:29:44 +0000

    Update AWS region and S3 bucket configurations

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-west-2"
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "jenkins-state-118"   
     key            = "terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-west-2"
     encrypt        = true
   }
 }
 resource "aws_s3_bucket" "example" {
-bucket = "jenkins-test-bucket-{yournamehere}"
+bucket = "jenkins-test-bucket-118118"
 acl    = "private"
 }
